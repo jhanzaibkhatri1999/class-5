e0269f2c7f893b574605d4ea56833f6e09277a58
jhanzeb.khatri1561@gmail.com <jhanzeb.khatri1561@gmail.com>
2023-06-23 22:51:27 +0500

    class 5

---
+++ map.dart
@@ -0,0 +1,18 @@
+void main() {
+
+//forEach
+//map
+// where
+//contains
+//firstWhere
+
+List studentNames = [
+
+{"name": "Jahanzaib", "marks": 85},
+{"name": "Azhar", "marks": 75}
+
+];
+
+print(studentNames[0]["name"]);
+
+}     
